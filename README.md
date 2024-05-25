6a33d87a8010da8b078515ab76aa72cfd5851a07
Manuel Ruffieux <manuel42994299@gmail.com>
2024-05-25 12:12:26 +0300

    removed uneccessary stuff, downgrade to .net 6

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Type-language-notifier
 A project that will run in the background and notify you what language you are typing at when starting.
+
+
+run 
+dotnet add package Gma.System.MouseKeyHook
--- main.cs
+++ main.cs
@@ -1,9 +1,7 @@
 using System;
-using System.IO;
 using System.Media;
-using System.Threading;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
+using Gma.System.MouseKeyHook;
 
 class KeyPressListener
 {
@@ -13,31 +11,55 @@ class KeyPressListener
     // Record the time of the last key press
     private static long lastKeyPressTime = 0;
     // Define the delay in milliseconds (30 seconds)
-    private static readonly long delay = TimeSpan.FromSeconds(30).Milliseconds;
+    private static readonly long delay = (long)TimeSpan.FromSeconds(30).TotalMilliseconds;
+
+    private static IKeyboardMouseEvents _hook;
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Key Press Listener started. Press any key to exit.");
-        // Gets the current input language.
-        InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
+        Console.WriteLine("Key Press Listener started. Press 'Esc' to exit.");
 
-        if(myCurrentLanguage != null) 
-            Console.WriteLine("Layout: " + myCurrentLanguage.LayoutName);
-        else
-            Console.WriteLine("There is no current language");        
-
-        // Start listening for key presses
-        HookKeyboard();
+        // Set up the global keyboard hook
+        _hook = Hook.GlobalEvents();
+        // Subscribe to the KeyDown event
+        _hook.KeyDown += KeyDown;
 
         // Keep the application running
-        Console.ReadLine();
-    }
+        while (true)
+        {
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+        }
 
+        // Unsubscribe from the event and dispose the hook
+        _hook.KeyDown -= KeyDown;
+        _hook.Dispose();
+    }
+    private static void KeyDown(object sender, KeyEventArgs e)
+    {
+        long currentTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        // Check if enough time has passed since the last key press
+        if (currentTime - lastKeyPressTime >= delay)
+        {
+            // Check what language is currently selected
+            InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
+            string language = myCurrentLanguage.LayoutName;
+            if (language == "ארצות הברית" || language == "United States")
+                PlaySound(englishPath);
+            else
+                PlaySound(hebrewPath);
+            
+            // Update the last key press time
+            lastKeyPressTime = currentTime;
+        }
+    }
 
     /**
      * Plays a sound from the specified file location.
      *
-     * @param language the file location of the sound to be played
+     * @param languagePath the file location of the sound to be played
      */
     public static void PlaySound(string languagePath)
     {
@@ -53,93 +75,4 @@ class KeyPressListener
             Console.WriteLine("Error with playing sound: " + e.Message);
         }
     }
-
-    public static void HookKeyboard()
-    {
-        // Create a new keyboard hook
-        KeyboardHook hook = new KeyboardHook();
-        // Register the key down event handler
-        hook.KeyDown += (sender, e) =>
-        {
-            long currentTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-            // Check if enough time has passed since the last key press
-            if (currentTime - lastKeyPressTime >= delay)
-            {
-                // check what language is currently selected
-                InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
-                string language = myCurrentLanguage.LayoutName;
-                if(language == "ארצות הברית" || language == "United States")
-                    PlaySound(englishPath);
-                else
-                    PlaySound(hebrewPath);   
-                PlaySound(language);
-                // Update the last key press time
-                lastKeyPressTime = currentTime;
-            }
-        };
-        // Install the keyboard hook
-        hook.Install();
-    }
-}
-
-class KeyboardHook : IDisposable
-{
-    private const int WH_KEYBOARD_LL = 13;
-    private const int WM_KEYDOWN = 0x0100;
-    private LowLevelKeyboardProc _proc;
-    private IntPtr _hookID = IntPtr.Zero;
-
-    public event EventHandler<KeyEventArgs> KeyDown;
-
-    public KeyboardHook()
-    {
-        _proc = HookCallback;
-    }
-
-    public void Install()
-    {
-        _hookID = SetHook(_proc);
-    }
-
-    public void Dispose()
-    {
-        UnhookWindowsHookEx(_hookID);
-    }
-
-    private IntPtr SetHook(LowLevelKeyboardProc proc)
-    {
-        using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
-        using (var curModule = curProcess.MainModule)
-        {
-            return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-                GetModuleHandle(curModule.ModuleName), 0);
-        }
-    }
-
-    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-    {
-        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-        {
-            int vkCode = Marshal.ReadInt32(lParam);
-            KeyEventArgs args = new KeyEventArgs((Keys)vkCode);
-            KeyDown?.Invoke(this, args);
-        }
-        return CallNextHookEx(_hookID, nCode, wParam, lParam);
-    }
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr SetWindowsHookEx(int idHook,
-        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-        IntPtr wParam, IntPtr lParam);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr GetModuleHandle(string lpModuleName);
 }
